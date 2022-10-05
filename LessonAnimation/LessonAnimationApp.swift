11b3de48be3f5333e81028222929f201fb4fe838
gaidaasd00 <swiftgaida@list.ru>
2022-10-05 20:33:11 +0300

    Initial Commit

---
+++ LessonAnimation/LessonAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonAnimationApp.swift
+//  LessonAnimation
+//
+//  Created by Алексей Гайдуков on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct LessonAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
