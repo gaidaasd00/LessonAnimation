6541dad44464c5a10cdb5b347432817614736b03
gaidaasd00 <swiftgaida@list.ru>
2022-10-08 12:05:00 +0300

    1.Added starter view

---
+++ LessonAnimation/StarterView.swift
@@ -0,0 +1,36 @@
+//
+//  StarterView.swift
+//  LessonAnimation
+//
+//  Created by Алексей Гайдуков on 08.10.2022.
+//
+
+import SwiftUI
+
+struct StarterView: View {
+    var body: some View {
+        TabView {
+            MainView()
+            .tabItem {
+                Image(systemName: "rosette")
+                Text("Main")
+            }
+            CartReacingView()
+                .tabItem {
+                    Image(systemName: "cart")
+                    Text("CartReacing")
+                }
+            AvardsView()
+                .tabItem {
+                    Image(systemName: "ruler.fill")
+                    Text("Awords")
+                }
+        }
+    }
+}
+
+struct StarterView_Previews: PreviewProvider {
+    static var previews: some View {
+        StarterView()
+    }
+}
